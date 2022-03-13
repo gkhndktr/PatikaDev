dc38e85143c5a51cea7e6ba23ae27eec7697fe19
Gökhan Döktür <gokhandoktur@gmail.com>
2022-03-13 23:34:24 +0300

    Methods added

---
+++ Methods/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //erisim_belirteci geri_donustipi metod_adi(parametreListesi/arguman);
+            //{
+            //  komutlar;
+            // return;
+            //}
+
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a+b);
+
+
+            int sonuc = Topla(a,b);
+            Console.WriteLine(sonuc);
+
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdır(Convert.ToString(sonuc));
+
+
+            int sonuc2 = ornek.ArttırVeTopla(ref a, ref b);
+            ornek.EkranaYazdır(Convert.ToString(sonuc2));
+            ornek.EkranaYazdır(Convert.ToString(a + b));
+
+
+        }
+        //Aynı klas içerisinde farklı bir motot kullanarak işlemimizi tamamlayacağız.
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdır(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttırVeTopla(ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+
+            return deger1+deger2;
+        }
+    }
+}
