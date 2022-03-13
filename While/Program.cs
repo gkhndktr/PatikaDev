43e573b5bbdbe4555a822b4cabc5e5c6dec8555b
Gökhan Döktür <gokhandoktur@gmail.com>
2022-03-13 12:26:41 +0300

    while project added

---
+++ While/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While 
+            //1'den başlayarak console'dan girilen sayıya kadar (sayı dahil)
+            Console.WriteLine("Bir sayı giriniz.");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac ++;
+                
+            }
+            
+            Console.WriteLine(toplam / sayi);
+
+            //a'dan z'ye kadar olan tüm harfleri console'a yazdır.
+
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.WriteLine(character);
+                character ++;
+            }
+
+            Console.WriteLine("*****Foreach****");
+            string [] arabalar = {"BMW","FORD","TOYOYA","NISSAN"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+
+        }
+    }
+}
