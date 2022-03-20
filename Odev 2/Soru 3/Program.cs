690090a05f263d94a814f44eea5a9af2c12c7427
Gökhan Döktür <gokhandoktur@gmail.com>
2022-03-20 21:50:29 +0300

    Odev 2 Soru 3 eklendi

---
+++ Odev 2/Soru 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+namespace Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri 
+            bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız. */
+            Console.WriteLine("Lütfen bir cümle giriniz!");
+            string check = Console.ReadLine();
+            ArrayList liste = new ArrayList();
+
+            foreach (var item in check)
+            {
+                if(item != ' ')
+                {
+                    if ("aeıioöuü".Contains(item))
+                    {
+                        liste.Add(item);
+                    }
+                }
+            }
+            liste.Sort();
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
