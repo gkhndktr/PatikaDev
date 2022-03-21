892e6b546e90cbacec229effd6a98c788687a034
Gökhan Döktür <gokhandoktur@gmail.com>
2022-03-21 22:05:26 +0300

    constructor added

---
+++ Constructor/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Söz Dizimi
+            //class SınıfAdı
+            //{
+            //  [erişim belirleyici] [veri tipi ] özellik adı;
+            //  [erişim belirletici] [geri dönüş tipi] MetotAdi([Parametre listesi])
+            //  {
+            //  //Metot komutları
+            //  }
+            //}
+
+
+            // Erişim belirleyiciler
+            //*Public
+            //*Private
+            //*Internal
+            //*Protocol
+
+            Calisan calisan1 = new Calisan();
+
+            calisan1.Adı = "Gökhan";
+            calisan1.Soyadı = "Döktür";
+            calisan1.No = 55555;
+            calisan1.Departman = "IT";
+            calisan1.CalisanBilgileri();
+            Console.WriteLine("**********");
+
+            Calisan calisan2 = new Calisan();
+            calisan2.Adı ="Deniz";
+            calisan2.Soyadı = "Döktür";
+            calisan2.No = 44444;
+            calisan2.Departman ="IT";
+            calisan2.CalisanBilgileri();
+            Console.WriteLine("**********");
+
+            Calisan calisan3 = new Calisan("Gökhan", "Döktür", 00000, "IT);
+            calisan3.CalisanBilgileri();
+            Console.WriteLine("**********");
+
+            Calisan calisan4 = new Calisan("Gökhan", "Döktür");
+            calisan4.CalisanBilgileri();
+        }
+
+        class Calisan
+        {
+            public string Adı;
+            public string Soyadı;
+            public int No;
+
+            public string Departman;
+
+            public Calisan(string ad, string soyad, int no, string departman)
+            {
+                this.Adı = ad;
+                this.Soyadı = soyad;
+                this.No = no;
+                this.Departman = departman;
+            }
+
+            public Calisan(string ad, string soyad)
+            {
+                this.Adı = ad;
+                this.Soyadı = soyad;
+            }
+
+            public Calisan(){}
+
+            public void CalisanBilgileri()
+            {
+                Console.WriteLine("Çalışanın adı:{0}", Adı);
+                Console.WriteLine("Çalışanın soyadı:{0}", Soyadı );
+                Console.WriteLine("Çalışanın numarası:{0}", No);
+                Console.WriteLine("Çalışanın departmanı:{0}", Departman);
+            }
+        }
+    }
+}
