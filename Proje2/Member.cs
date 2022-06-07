388506246c1c5f34f5b5262b3b5384d8e7be8929
Gökhan Döktür <gokhandoktur@gmail.com>
2022-06-07 22:22:36 +0300

    AddCard method is added

---
+++ Proje2/Member.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace Proje2
+{
+    public class Member
+    {
+        private string firstName;
+        private string lastName;
+        private int id;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Id { get => id; set => id = value; }
+        public Member(string firstname, string lastname, int id)
+         {
+            this.FirstName =firstName;
+            this.LastName = lastName;
+            this.Id = id;
+         }
+
+
+
+    }
+    public static class MemberModel
+    {
+        public static List<Member> distribution = new List<Member>();
+    }
+}
